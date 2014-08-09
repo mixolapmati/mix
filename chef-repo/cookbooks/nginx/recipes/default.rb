92a6853b57b8f30ba00958bd3ceb0294c798ca81
mixolapmati <mixolapmati@gmail.com>
2014-08-09 19:33:34 -0400

    recipe default.rb edited for nginx

--- chef-repo/cookbooks/nginx/recipes/default.rb
+++ chef-repo/cookbooks/nginx/recipes/default.rb
@@ -2,13 +2,13 @@ package "nginx" do
 	action :install
 end
 
-package "nginx-common" do
-        action :install
-end
+#apackage "nginx-common" do
+#        action :install
+#end
 
-package "nginx-full" do
-       actio :install
-end
+#package "nginx-full" do
+#       action :install
+#end
 
 template "/usr/share/nginx/www/index.html" do
 	source "index.html.erb"
