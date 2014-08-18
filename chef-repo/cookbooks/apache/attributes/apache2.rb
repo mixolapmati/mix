dba81e7547f807f8a19275cb6bc4634e40ca865f
mixolapmati <mixolapmati@gmail.com>
2014-08-18 01:03:00 -0400

    modified:   chef-repo/cookbooks/apache/attributes/apache2.rb 	modified:   chef-repo/roles/web.rb

--- chef-repo/cookbooks/apache/attributes/apache2.rb
+++ chef-repo/cookbooks/apache/attributes/apache2.rb
@@ -1,2 +1,1 @@
-node.default["apache2"]["dir"] = "/etc/apache2"
-node.default["apache2"]["listen_ports"] = ["8080"]
+default["apache2"]["listen_ports"] = ["8080"]
--- chef-repo/roles/web.rb
+++ chef-repo/roles/web.rb
@@ -1,4 +1,3 @@
 name "web"
 
-override_attributes "nginx" => {"listen_ports" => ["8080"]}
 run_list "recipe[nginx]", "recipe[apache]"
